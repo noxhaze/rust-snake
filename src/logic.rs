ae31f14b7dcab9893f2923827ad92fb5f48a4545
noxhaze <noxhaze@proton.me>
2024-05-26 17:30:00 -0400

    Update grid method implementation

--- src/logic.rs
+++ src/logic.rs
@@ -8,6 +8,21 @@ pub struct Grid {
 }
 
 impl Grid {
+    pub fn update(&mut self, snake: &Snake) {
+        self.array = Array2D::filled_by_row_major(
+            || renderer::BLANK,
+            self.width.try_into().unwrap(),
+            self.height.try_into().unwrap(),
+        );
+
+        for node in snake.nodes.iter() {
+            *self
+                .array
+                .get_mut(node.x.try_into().unwrap(), node.y.try_into().unwrap())
+                .expect("Out of bounds access of grid") = renderer::SNAKE;
+        }
+    }
+
     pub fn new(width: u32, height: u32) -> Self {
         Self {
             width,
@@ -29,8 +44,8 @@ pub enum Direction {
 }
 
 pub struct Position {
-    x: u32,
-    y: u32,
+    pub x: u32,
+    pub y: u32,
 }
 
 impl PartialEq for Position {
@@ -49,7 +64,7 @@ impl Clone for Position {
 }
 
 pub struct Snake {
-    nodes: Vec<Position>,
+    pub nodes: Vec<Position>,
     pub dir: Direction,
 }
 
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,22 @@ mod logic;
 mod renderer;
 
 fn main() {
-    let mut grid: logic::Grid = logic::Grid::new(10, 10);
+    let width: u32 = 10;
+    let height: u32 = 10;
 
-    renderer::render(&grid);
+    let mut grid: logic::Grid = logic::Grid::new(width, height);
+    let mut snake: logic::Snake = logic::Snake::new(
+        logic::Position {
+            x: width / 2,
+            y: height / 2,
+        },
+        logic::Direction::Down,
+    );
+
+    loop {
+        renderer::render(&grid);
+
+        grid.update(&snake);
+        snake.update();
+    }
 }
