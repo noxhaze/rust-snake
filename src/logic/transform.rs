60bd0246d822b31343cf1d3e996a1a9224bfb533
noxhaze <noxhaze@proton.me>
2024-05-26 21:22:23 -0400

    Implement std::fmt::Display trait

--- src/logic/transform.rs
+++ src/logic/transform.rs
@@ -24,3 +24,9 @@ impl Clone for Position {
         }
     }
 }
+
+impl std::fmt::Display for Position {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({}, {})", self.x, self.y)
+    }
+}
